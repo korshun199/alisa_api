a8e6e5287561bfd0d4a6a471fb8b858b37cce370
John Doe <johndoe@example.com>
2020-11-28 03:50:22 +0300

    first

---
+++ alisa/setting.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os, pytz, socket, dj_database_url, rich
+
+DEBUG = True
+ACCESS_USER = True
+
+
+SITE_URL = "http://127.0.0.1:ХУЙ-БУЙ"
+
+
+SITE_ID = 2
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
